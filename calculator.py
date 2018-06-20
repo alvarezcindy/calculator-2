c930516df879dd93f67ff05bdbfb880c3e168b52
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 16:04:54 -0700

    Added reduce function

--- calculator.py
+++ calculator.py
@@ -5,42 +5,36 @@ calculator program yourself in this file.
 """
 
 from arithmetic import *
+from functools import reduce
 
-
-# Your code goes here
 while True:
     input_string = input("> ")
     token = input_string.split(" ")
 
-    if len(token) > 3 or len(token) < 2:
-        print("Please input an operator and at least one number")
+    if token[0] == "q":
+        break
 
+    operator = token[0]
+
+    float_list = (float(i) for i in token[1:])
+
+    if operator == "pow":
+        answer = reduce(power, float_list)
+    elif operator == '+':
+        answer = reduce(add, float_list)
+    elif operator == '-':
+        answer = reduce(subtract, float_list)
+    elif operator == '*':
+        answer = reduce(multiply, float_list)
+    elif operator == '/':
+        answer = reduce(divide, float_list)
+    elif operator == 'square':
+        answer = reduce(square, float_list)
+    elif operator == 'cube':
+        answer = reduce(cube, float_list)
+    elif operator == 'mod':
+        answer = reduce(mod, float_list)
     else:
-        operator = token[0]
-        num1 = float(token[1])
-        num2 = float(token[2])
-
-        if operator == "q":
-            break
-        elif operator == "pow":
-            answer = power(num1, num2)
-        elif operator == '+':
-            answer = add(num1, num2)
-        elif operator == '-':
-            answer = subtract(num1, num2)
-        elif operator == '*':
-            answer = multiply(num1, num2)
-        elif operator == '/':
-            answer = divide(num1, num2)
-        elif operator == 'square':
-            answer = square(num1)
-        elif operator == 'cube':
-            answer = cube(num1)
-        elif operator == 'mod':
-            answer = mod(num1, num2)
-        else:
-            print("Please input an operator and at least one number")
-
-        print(answer)
-
+        print("Please input an operator and at least one number")
+    print(answer)
 print("Quitting...")
